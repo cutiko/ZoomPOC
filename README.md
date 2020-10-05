7f4b136610763045cd5c1e3435c1687a87fab545
Erick Navarro <cutiko@gmail.com>
2020-10-05 10:00:00 -0300

    PhotoView it works great but the zoom is image view localized

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # ZoomPOC
 
 First commit has the from scracth implementation, it seems doable with some more maths and case handling, but that would only be zoom. **Focalized pinch zoom would still be missing**.
+
+
+## PhotoView
+
+[Dependency Link](https://github.com/chrisbanes/PhotoView)
+
+Debug APK before adding the dependency is 4.2 MB total size
+
+After the library implementation the size is 4.4 MB
+
+**Problem: It only zoom the image view area it doesn't fill the entire layout container**
--- app/src/main/java/cl/cutiko/zoompoc/FirstFragment.kt
+++ app/src/main/java/cl/cutiko/zoompoc/FirstFragment.kt
@@ -1,13 +1,9 @@
 package cl.cutiko.zoompoc
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.ScaleGestureDetector
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_first.*
@@ -17,10 +13,6 @@ import kotlinx.android.synthetic.main.fragment_first.*
  */
 class FirstFragment : Fragment() {
 
-    private val scalings = mutableListOf<Float>()
-    private var lastScalingTime = 0L
-    //private var calculatedScaleFactor = SCALE_FACTOR
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,39 +21,9 @@ class FirstFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
-    //TODO support clicking?
-    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Picasso.get().load(URL).centerCrop().fit().into(pocImageView)
-        val scaleDetector = ScaleGestureDetector(requireContext(), object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
-            override fun onScale(detector: ScaleGestureDetector?): Boolean {
-                val eventHandled = super.onScale(detector)
-                detector?.scaleFactor?.let { scaleFactor ->
-                    Log.d("CUTIKO_TAG", "$scaleFactor" );
-                    val timeStamp = System.currentTimeMillis()
-                    if (timeStamp - lastScalingTime > COOL_DOWN) {
-                        lastScalingTime = timeStamp
-                        val lastScale = scalings.lastOrNull()
-                        scalings.add(scaleFactor)
-                        if (lastScale != null && lastScale < scaleFactor) {
-                            scaleImage(scaleFactor)
-                        }
-                    }
-                }
-                return eventHandled
-            }
-        })
-
-
-        pocImageView.setOnTouchListener { imageView, event ->
-            scaleDetector.onTouchEvent(event)
-            //imageView.performClick()
-            true
-        }
-        pocImageView.setOnClickListener {
-            Toast.makeText(requireContext(), "clicked", Toast.LENGTH_LONG).show()
-        }
     }
 
     private fun scaleImage(scaleFactor: Float) {
@@ -73,9 +35,5 @@ class FirstFragment : Fragment() {
 
     companion object {
         private const val URL = "https://unsplash.com/photos/qO-PIF84Vxg/download?force=true&w=640"
-        private const val COOL_DOWN = 120
-        /*private const val SCALE_FACTOR = 1.0F
-        private const val MIN_SCALE_FACTOR = 0.1F
-        private const val MAX_SCALE_FACTOR = 10.0F*/
     }
 }
